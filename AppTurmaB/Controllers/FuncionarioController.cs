b72482e835924f4ea9082534243bd3105fa563de
SayArthur <arthurbmoreno@yahoo.com.br>
2022-05-09 09:30:11 -0300

    Adicionar arquivos de projeto.

---
+++ AppTurmaB/Controllers/FuncionarioController.cs
@@ -0,0 +1,99 @@
+﻿using AppTurmaB.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AppTurmaB.Controllers
+{
+    public class FuncionarioController : Controller
+    {
+        // GET: Funcionario
+        public ActionResult Index(Funcionario funcionario)
+        {
+            return View(funcionario);
+        }
+
+        // GET: Funcionario/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Funcionario/Create
+        [HttpPost]
+        public ActionResult Create(Funcionario funcionario)
+        {
+            if (ModelState.IsValid)
+                return RedirectToAction("Index", funcionario);
+            return View();
+        }
+
+        public ActionResult ValidaLogin(string login)
+        {
+            var dbexemplo = new Collection<String>()
+            {
+                "Enildo 01",
+                "Enildo 02",
+                "Enildo 03",
+                "Enildo 04",
+                "Enildo 05"
+            };
+            return Json(dbexemplo.All(
+                                        a => a.ToLower() != login.ToLower()), 
+                                        JsonRequestBehavior.AllowGet
+                                     );
+        }
+
+        //// GET: Funcionario/Edit/5
+        //public ActionResult Edit(int id)
+        //{
+        //    return View();
+        //}
+        // GET: Funcionario/Details/5
+        //public ActionResult Details(int id)
+        //{
+        //    return View();
+        //}
+
+        //// POST: Funcionario/Edit/5
+        //[HttpPost]
+        //public ActionResult Edit(int id, FormCollection collection)
+        //{
+        //    try
+        //    {
+        //        // TODO: Add update logic here
+
+        //        return RedirectToAction("Index");
+        //    }
+        //    catch
+        //    {
+        //        return View();
+        //    }
+        //}
+
+        //// GET: Funcionario/Delete/5
+        //public ActionResult Delete(int id)
+        //{
+        //    return View();
+        //}
+
+        //// POST: Funcionario/Delete/5
+        //[HttpPost]
+        //public ActionResult Delete(int id, FormCollection collection)
+        //{
+        //    try
+        //    {
+        //        // TODO: Add delete logic here
+
+        //        return RedirectToAction("Index");
+        //    }
+        //    catch
+        //    {
+        //        return View();
+        //    }
+        //}
+    }
+}
---
+++ AppTurmaB/Models/Funcionario.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AppTurmaB.Models
+{
+    public class Funcionario
+    {
+        [Range(1, 200, ErrorMessage = "Limite entre 1 e 200")]
+        [Display(Name = "Código")]
+        [Required(ErrorMessage = "Campo obrigatório")]
+        public int FuncID { get; set; }
+
+        [StringLength(80, MinimumLength = 2, ErrorMessage = "Deve ter entre 2 e 80 caracteres")]
+        [Required(ErrorMessage = "Campo obrigatório")]
+        public string Nome { get; set; }
+        
+        [EmailAddress]
+        [RegularExpression(@"^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$", ErrorMessage = "Formato de email inválido")]
+        [Display(Name = "Email")]
+        [Required(ErrorMessage = "Campo obrigatório")]
+        [DataType(DataType.EmailAddress, ErrorMessage = "Formato de email inválido")]
+        public string Email { get; set; }
+
+        [Display(Name = "Data de nascimento")]
+        [Required(ErrorMessage = "Campo obrigatório")]
+        [DataType(DataType.Date, ErrorMessage = "Formato de data inválido")]
+        public DateTime DataNasc { get; set; }
+
+        [StringLength(1)]
+        [Required(ErrorMessage = "Campo obrigatório")]
+        public string Sexo { get; set; }
+
+        [Display(Name = "Login")]
+        [Required(ErrorMessage = "Campo obrigatório")]
+        [Remote("ValidaLogin", "Funcionario", ErrorMessage = "Login já existe.")]
+        public string Login { get; set; }
+
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage = "Campo obrigatório")]
+        [RegularExpression(@"[a-zA-Z]{5,10}", ErrorMessage = "Só letras")]
+        public string Senha { get; set; }
+
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage = "Campo obrigatório")]
+        [System.ComponentModel.DataAnnotations.Compare("Senha")]
+        [Display(Name = "Confirmar senha")]
+        public string ConfirmarSenha { get; set; }
+    }
+}
